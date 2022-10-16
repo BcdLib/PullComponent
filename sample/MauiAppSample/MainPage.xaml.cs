99c98aa5ea5efadb97be176912aed551c15ac28f
zxyao <zxyao145@gmail.com>
2022-10-16 16:33:47 +0800

    feat: fix dotnet build cannot include staticwebassets
    https://github.com/dotnet/sdk/issues/3065

---
+++ sample/MauiAppSample/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiAppSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ sample/MauiAppSample/MauiProgram.cs
@@ -0,0 +1,30 @@
+﻿using BcdLib.Components;
+using MauiAppSample.Data;
+using Microsoft.AspNetCore.Components.WebView.Maui;
+
+namespace MauiAppSample
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                });
+
+            builder.Services.AddMauiBlazorWebView();
+#if DEBUG
+		builder.Services.AddBlazorWebViewDeveloperTools();
+#endif
+
+            builder.Services.AddSingleton<WeatherForecastService>();
+            builder.Services.AddBcdLibPullComponent();
+
+            return builder.Build();
+        }
+    }
+}
